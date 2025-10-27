7df30480ebc6155946e1f9ec3bcd8d360a9cfadb
trandinh0506 <tvandinh93@gmail.com>
2025-10-27 20:25:44 +0700

    move repo

--- frontend/app/trends/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '../ui/dashboardLayout';
-
-export default function Trend() {
-  return (
-    <DashboardLayout>
-      <div>this is trending playlists</div>
-    </DashboardLayout>
-  );
-}
